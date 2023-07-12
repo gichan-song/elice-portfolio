cb147ccd19b7db9cb0724a11eb60d4dd14d34353
euiho.kim <kim-eh@naver.com>
2023-07-12 21:21:00 +0900

    feat: 사용자 인증 및 라우터를 위한 코드 추가

---
+++ src/components/routes/Router.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextStore } from '../context/AuthContext';
+
+import NonAuthRoute from './NonAuthRoute';
+import AuthRoute from './AuthRoute';
+import HomePage from './../../pages/HomePage/HomePage';
+import JoinPage from '../../pages/JoinPage/JoinPage';
+import LoginPage from '../../pages/LoginPage/LoginPage';
+import NotFoundpage from '../../pages/NotFoundPage/NotFoundPage';
+
+const Router = () => {
+  const { userToken } = useContext(AuthContextStore);
+
+  return (
+    <Routes>
+      <Route path='*' element={<NotFoundpage />} />
+      <Route path='/notfound' element={<NotFoundpage />} />
+      <Route path='/' element={<HomePage />} />
+
+      <Route element={<NonAuthRoute authenticated={userToken} />}>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/join' element={<JoinPage />} />
+      </Route>
+
+      <Route element={<AuthRoute authenticated={userToken} />}></Route>
+    </Routes>
+  );
+};
+
+export default Router;
