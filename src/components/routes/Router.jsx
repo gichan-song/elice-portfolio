f427dc2210cbf698b3edd06456fc239c9425491b
euiho.kim <kim-eh@naver.com>
2023-07-20 23:44:13 +0900

    feat: 게시글 업로드에 대한 코드 추가

--- src/components/routes/Router.jsx
+++ src/components/routes/Router.jsx
@@ -8,6 +8,8 @@ import HomePage from './../../pages/HomePage/HomePage';
 import JoinPage from '../../pages/JoinPage/JoinPage';
 import LoginPage from '../../pages/LoginPage/LoginPage';
 import NotFoundpage from '../../pages/NotFoundPage/NotFoundPage';
+import TestPage from '../../pages/TestPage/TestPage';
+import PostUploadPage from '../../pages/postPage/PostUploadPage/PostUploadPage';
 
 const Router = () => {
   const { userToken } = useContext(AuthContextStore);
@@ -17,10 +19,12 @@ const Router = () => {
       <Route path='*' element={<NotFoundpage />} />
       <Route path='/notfound' element={<NotFoundpage />} />
       <Route path='/' element={<HomePage />} />
+      <Route path='/test' element={<TestPage />} />
 
       <Route element={<NonAuthRoute authenticated={userToken} />}>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/join' element={<JoinPage />} />
+        <Route path='/post' element={<PostUploadPage />} />
       </Route>
 
       <Route element={<AuthRoute authenticated={userToken} />}></Route>
