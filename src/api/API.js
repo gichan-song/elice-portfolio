0d27d679952e4c54553c6d3787c116222398542c
euiho kim <ehkim@elicer.com>
2023-08-10 17:55:10 +0900

    feat: API endpoint 분리

---
+++ src/api/API.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+// const serverURL = `${window.location.origin}`;
+const serverURL = 'http://localhost:4000';
+
+const API = async (endpoint, method, data) => {
+  const token = localStorage.getItem('token');
+  const headers = {
+    'Content-type': 'application/json',
+  };
+  // 토큰이 존재한다면,
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  const options = {
+    url: serverURL + endpoint,
+    method: method,
+    headers: headers,
+    data: data,
+  };
+
+  try {
+    const response = await axios(options);
+    return response;
+  } catch (error) {
+    if (error.response) {
+      console.log('Error status:', error.response.status);
+    }
+    throw error;
+  }
+};
+
+export default API;
---
+++ src/api/ENDPOINT.js
@@ -0,0 +1,10 @@
+const ENDPOINT = {
+  EMAIL_DUPLICATE_CHECK: '/users/validate/id',
+  NICKNAME_DUPLICATE_CHECK: '/users/validate/nickname',
+  JOIN: '/users/signup',
+  LOGIN: '/users/login',
+  GET_USER_INFO: '/users/profile',
+  EDIT_USER_PROFILE: '/users/profile',
+};
+
+export default ENDPOINT;
