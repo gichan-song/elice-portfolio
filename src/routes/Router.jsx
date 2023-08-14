8523b3799c51f3df44c6c15caefd7d751591de29
gichan <gichan@gichanui-MacBookPro.local>
2023-08-14 11:53:01 +0900

    add: 충돌 오류 해결 중 삭제된 파일 Router.jsx

---
+++ src/routes/Router.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextStore } from '../context/AuthContext';
+
+import NonAuthRoute from './NonAuthRoute';
+import AuthRoute from './AuthRoute';
+import HomePage from '../pages/HomePage/HomePage';
+import JoinPage from '../pages/JoinPage/JoinPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import NotFoundpage from '../pages/NotFoundPage/NotFoundPage';
+import TestPage from '../pages/TestPage/TestPage';
+import PostUploadPage from '../pages/postPage/PostUploadPage/PostUploadPage';
+import PostDetailPage from '../pages/postPage/postDetailPage/PostDetailPage';
+import PostEditPage from '../pages/postPage/PostEditPage/PostEditPage';
+import ProfileSettingPage from '../pages/profileSettingPage/ProfileSettingPage';
+import GuidePage from '../pages/guidePage/GuidePage';
+
+const Router = () => {
+  const { token } = useContext(AuthContextStore);
+
+  return (
+    <Routes>
+      <Route path='*' element={<NotFoundpage />} />
+      <Route path='/notfound' element={<NotFoundpage />} />
+      <Route path='/' element={<HomePage />} />
+      {/* <Route path='/test' element={<TestPage />} /> */}
+
+      <Route element={<NonAuthRoute authenticated={token} />}>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/join' element={<JoinPage />} />
+      </Route>
+
+      <Route element={<AuthRoute authenticated={token} />}>
+        <Route path='/post' element={<PostUploadPage />} />
+        <Route path='/post/:postid' element={<PostDetailPage />} />
+        <Route path='/post/edit/:postid' element={<PostEditPage />} />
+        {/* <Route path='/profile/:nickname' element={<ProfileSettingPage />} /> */}
+        <Route path='/profile' element={<ProfileSettingPage />} />
+        <Route path='/guide' element={<GuidePage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Router;
