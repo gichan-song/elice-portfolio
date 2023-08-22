63afe448614ccc1c5d8dab2289d33d570e7feedb
gichan <gichan@gichanui-MacBookPro.local>
2023-08-22 10:03:33 +0900

    fix: add asynchandler

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -20,71 +20,84 @@ router.post('/signup', (req, res) => {
 });
 
 // 이메일 중복확인 API
-router.post('/validate/id/:id', async (req, res) => {
-  const { id } = req.params;
+router.post(
+  '/validate/id/:id',
+  asynchandler(async (req, res) => {
+    const { id } = req.params;
 
-  if (await User.exists({ id })) {
-    res.status(409).json({ message: 'Id exists' });
-    return;
-  }
-  res.json({ message: 'Id available' });
-});
-
-//닉네임 중복확인 API
-router.post('/validate/nickname/:nickname', async (req, res) => {
-  const { nickname } = req.params;
-
-  if (await User.exists({ nickname })) {
-    res.status(409).json({ message: 'Nickname exists' });
-    return;
-  }
-  res.json({ message: 'Nickname available' });
-});
-
-//로그인 API
-router.post('/login', async (req, res) => {
-  const { id, password } = req.body;
-  const user = await User.findOne({ id: id });
-
-  if (!user) {
-    res.status(401).json({ message: 'User not found' });
-    return;
-  }
-
-  if (user.password !== hashPassword(password)) {
-    res.status(401).json({ message: 'Password is incorrect' });
-    return;
-  }
-  const payload = {
-    _id: user._id,
-  };
-
-  jwt.sign(payload, 'secret', { expiresIn: '1h' }, (err, token) => {
-    res.json({ token: token });
-  });
-});
-
-// 프로필 조회 API
-router.get('/profile', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secret', async (err, authData) => {
-    if (err) {
-      res.status(403).json({ message: 'Login required' });
+    if (await User.exists({ id })) {
+      res.status(409).json({ message: 'Id exists' });
       return;
     }
-    const user = await User.findById(authData._id).populate('scraps');
+    res.json({ message: 'Id available' });
+  }),
+);
+
+//닉네임 중복확인 API
+router.post(
+  '/validate/nickname/:nickname',
+  asynchandler(async (req, res) => {
+    const { nickname } = req.params;
+
+    if (await User.exists({ nickname })) {
+      res.status(409).json({ message: 'Nickname exists' });
+      return;
+    }
+    res.json({ message: 'Nickname available' });
+  }),
+);
+
+//로그인 API
+router.post(
+  '/login',
+  asynchandler(async (req, res) => {
+    const { id, password } = req.body;
+    const user = await User.findOne({ id: id });
 
     if (!user) {
       res.status(401).json({ message: 'User not found' });
       return;
     }
 
-    res.json({
-      id: user.id,
-      nickname: user.nickname,
-      profileImg: user.profileImg,
-      scraps: user.scraps,
+    if (user.password !== hashPassword(password)) {
+      res.status(401).json({ message: 'Password is incorrect' });
+      return;
+    }
+    const payload = {
+      _id: user._id,
+    };
+
+    jwt.sign(payload, 'secret', { expiresIn: '1h' }, (err, token) => {
+      res.json({ token: token });
     });
-  });
+  }),
+);
+
+// 프로필 조회 API
+router.get('/profile', verifyToken, (req, res) => {
+  jwt.verify(
+    req.token,
+    'secret',
+    asynchandler(async (err, authData) => {
+      if (err) {
+        res.status(403).json({ message: 'Login required' });
+        return;
+      }
+      const user = await User.findById(authData._id).populate('scraps');
+
+      if (!user) {
+        res.status(401).json({ message: 'User not found' });
+        return;
+      }
+
+      res.json({
+        id: user.id,
+        nickname: user.nickname,
+        profileImg: user.profileImg,
+        scraps: user.scraps,
+      });
+    }),
+  );
 });
 
 // 유저 검색 및 조회 API
@@ -107,84 +120,92 @@ router.get(
 
 // 프로필 수정 API
 router.put('/profile', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secret', async (err, authData) => {
-    if (err) {
-      res.status(403).json({ message: 'Login required' });
-      return;
-    }
+  jwt.verify(
+    req.token,
+    'secret',
+    asynchandler(async (err, authData) => {
+      if (err) {
+        res.status(403).json({ message: 'Login required' });
+        return;
+      }
 
-    const { nickname, profileImg, currentpassword, changedpassword } = req.body;
+      const { nickname, profileImg, currentpassword, changedpassword } = req.body;
 
-    const user = await User.findById(authData._id);
+      const user = await User.findById(authData._id);
 
-    if (!user) {
-      res.status(401).json({ message: 'User not found' });
-      return;
-    }
-    if (hashPassword(currentpassword) !== user.password) {
-      res.status(401).json({ message: 'Password is incorrect' });
-      return;
-    }
-    user.nickname = nickname;
-    user.profileImg = profileImg;
+      if (!user) {
+        res.status(401).json({ message: 'User not found' });
+        return;
+      }
+      if (hashPassword(currentpassword) !== user.password) {
+        res.status(401).json({ message: 'Password is incorrect' });
+        return;
+      }
+      user.nickname = nickname;
+      user.profileImg = profileImg;
 
-    if (changedpassword !== '' && changedpassword !== undefined) {
-      user.password = hashPassword(changedpassword);
-    }
+      if (changedpassword !== '' && changedpassword !== undefined) {
+        user.password = hashPassword(changedpassword);
+      }
 
-    user.save();
+      user.save();
 
-    res.json({
-      id: user.id,
-      nickname: user.nickname,
-      profileImg: user.profileImg,
-    });
-  });
+      res.json({
+        id: user.id,
+        nickname: user.nickname,
+        profileImg: user.profileImg,
+      });
+    }),
+  );
 });
 
 // 스크랩 API
 router.post('/:postId', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secret', async (err, authData) => {
-    if (err) {
-      res.status(403).json({ message: 'Login required' });
-      return;
-    }
-
-    const { postId } = req.params;
-
-    const user = await User.findById(authData._id);
-
-    if (!user) {
-      res.status(401).json({ message: 'User not found' });
-      return;
-    }
-
-    const post = await Post.findById(postId);
-
-    if (!post) {
-      res.status(404).json({ message: 'Post not found' });
-      return;
-    }
-
-    const scraps = user.scraps;
-    let flag = false;
-
-    for (let i = 0; i < scraps.length; i++) {
-      if (scraps[i].toString() === postId) {
-        user.scraps.pull(postId);
-        flag = true;
-        break;
+  jwt.verify(
+    req.token,
+    'secret',
+    asynchandler(async (err, authData) => {
+      if (err) {
+        res.status(403).json({ message: 'Login required' });
+        return;
       }
-    }
 
-    if (!flag) {
-      user.scraps.push(postId);
-    }
+      const { postId } = req.params;
 
-    user.save();
+      const user = await User.findById(authData._id);
 
-    res.json(user.scraps);
-  });
+      if (!user) {
+        res.status(401).json({ message: 'User not found' });
+        return;
+      }
+
+      const post = await Post.findById(postId);
+
+      if (!post) {
+        res.status(404).json({ message: 'Post not found' });
+        return;
+      }
+
+      const scraps = user.scraps;
+      let flag = false;
+
+      for (let i = 0; i < scraps.length; i++) {
+        if (scraps[i].toString() === postId) {
+          user.scraps.pull(postId);
+          flag = true;
+          break;
+        }
+      }
+
+      if (!flag) {
+        user.scraps.push(postId);
+      }
+
+      user.save();
+
+      res.json(user.scraps);
+    }),
+  );
 });
 
 module.exports = router;
