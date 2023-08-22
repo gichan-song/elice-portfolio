e49b7741c7641fbb33888daf7b4bdb1aa8c5f5e0
euiho kim <ehkim@elicer.com>
2023-08-22 18:39:20 +0900

    feat: 댓글 기능 추가

--- src/pages/postPage/postDetailPage/Comment/Comment.jsx
+++ src/pages/postPage/postDetailPage/Comment/Comment.jsx
@@ -1,42 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SubHeadingLayout from '../../../../components/common/layout/SubHeadingLayout/SubHeadingLayout';
 import { styled } from 'styled-components';
 import profileIcon from '../../../../assets/icons/profile-icon.svg';
 import deleteFillIcon from '../../../../assets/icons/delete-fill-icon.svg';
+import API from '../../../../api/API';
+import ENDPOINT from '../../../../api/ENDPOINT';
+import Button from '../../../../components/common/Button/Button';
 
-const Comment = () => {
-  const handleRemove = () => {
+const Comment = ({ postInfo, getPostInfo }) => {
+  // 댓글 작성 시 기능구현
+  const [comment, setComment] = useState('');
+
+  const HandleCommentInput = (e) => {
+    setComment(e.target.value);
+  };
+
+  const HandleWrite = async () => {
+    if (!comment.trim().length > 0) {
+      alert('의미 있는 댓글을 작성해 주세요.');
+      setComment('');
+      return;
+    }
+
+    await API(`${ENDPOINT.POSTS}/${postInfo._id}/comments`, 'POST', { comment: comment })
+      .then((res) => {
+        console.log(res);
+        setComment('');
+        setTimeout(() => {
+          getPostInfo();
+        }, 100);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // 엔터 키를 눌렀을 때 댓글 작성 클릭과 동일한 효과를 주기 위한 함수
+  const onEnterKeyDownHandler = (e) => {
+    if (e.key === 'Enter') {
+      HandleWrite();
+    }
+  };
+
+  const handleRemove = async (commentId) => {
     console.log('댓글 삭제 기능이 들어갈 곳입니다.');
+
+    await API(`${ENDPOINT.POSTS}/${postInfo._id}/comments/${commentId}`, 'DELETE')
+      .then((res) => {
+        console.log(res);
+        setComment('');
+        setTimeout(() => {
+          getPostInfo();
+        }, 100);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.data.message === 'User not authorized') {
+          return alert('자신의 댓글만 삭제할 수 있습니다.');
+        }
+      });
   };
 
   return (
     <>
-      <SubHeadingLayout subHeadingName='댓글'>
+      <SubHeadingLayout subHeadingName={`댓글 ${postInfo.comments?.length}`}>
         <CommentLayout>
-          <CommentContainer>
-            <ProfileInfoContainer>
-              <ProfileInfo>
-                <ProfileImg src={profileIcon} alt='프로필 이미지' />
-                <UserName>{'user1'}</UserName>
-              </ProfileInfo>
-              <DeleteImg src={deleteFillIcon} alt='삭제 아이콘' onClick={handleRemove} />
-            </ProfileInfoContainer>
-            <CommentContent>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. `}</CommentContent>
-            <DateInfo>{'22분 전'}</DateInfo>
-          </CommentContainer>
-
-          <CommentContainer>
-            <ProfileInfoContainer>
-              <ProfileInfo>
-                <ProfileImg src={profileIcon} alt='프로필 이미지' />
-                <UserName>{'user2'}</UserName>
-              </ProfileInfo>
-              <DeleteImg src={deleteFillIcon} alt='삭제 아이콘' onClick={handleRemove} />
-            </ProfileInfoContainer>
-            <CommentContent>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. `}</CommentContent>
-            <DateInfo>{'1시간 전'}</DateInfo>
-          </CommentContainer>
+          {postInfo.comments &&
+            postInfo.comments.map((item) => (
+              <CommentContainer key={item._id}>
+                <ProfileInfoContainer>
+                  <ProfileInfo>
+                    <ProfileImg src={item.userProfileImg ? item.userProfileImg : profileIcon} alt='프로필 이미지' />
+                    <UserName>{item.userNickname}</UserName>
+                  </ProfileInfo>
+                  {
+                    <DeleteImg
+                      src={deleteFillIcon}
+                      alt='삭제 아이콘'
+                      onClick={() => {
+                        handleRemove(item._id);
+                      }}
+                    />
+                  }
+                </ProfileInfoContainer>
+                <CommentContent>{item.comment}</CommentContent>
+                <DateInfo>{item.date}</DateInfo>
+              </CommentContainer>
+            ))}
+          {postInfo.comments?.length === 0 && <span>작성된 댓글이 없습니다.</span>}
         </CommentLayout>
+        <CommentInputWrapper>
+          <CommentInput
+            type='text'
+            placeholder='댓글 작성'
+            onChange={HandleCommentInput}
+            value={comment}
+            maxLength={600}
+            onKeyDown={onEnterKeyDownHandler}
+          />
+          <Button type='comment' onClickHandler={HandleWrite}>
+            댓글 작성
+          </Button>
+        </CommentInputWrapper>
       </SubHeadingLayout>
     </>
   );
@@ -73,6 +138,8 @@ const ProfileInfo = styled.div`
 const ProfileImg = styled.img`
   width: 2rem;
   height: 2rem;
+  border-radius: 50%;
+  object-fit: cover;
 `;
 
 const UserName = styled.span`
@@ -92,4 +159,25 @@ const CommentContent = styled.p`
 
 const DateInfo = styled.span`
   color: var(--text-color);
+`;
+
+const CommentInputWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 2rem;
+`;
+
+const CommentInput = styled.input`
+  width: 100%;
+  height: 4rem;
+  border: 2px solid var(--border-color);
+  font-size: var(--fs-md);
+  padding: 1.5rem;
+  border-radius: 8px;
+  &:focus {
+    outline: none;
+    border: 2px solid var(--main-color);
+  }
 `;
