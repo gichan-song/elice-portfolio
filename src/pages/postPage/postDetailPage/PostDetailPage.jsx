08013cf625ebe1bd3aa28e5b03694f62de19fddb
euiho kim <ehkim@elicer.com>
2023-08-22 18:38:47 +0900

    feat: 게시물 상세 페이지에 대한 코드 추가

--- src/pages/postPage/postDetailPage/PostDetailPage.jsx
+++ src/pages/postPage/postDetailPage/PostDetailPage.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from 'react';
-import { styled } from 'styled-components';
+import React, { useCallback, useEffect, useState } from 'react';
 import MainHeadingLayout from '../../../components/common/layout/MainHeadingLayout/MainHeadingLayout';
 import Intro from './Intro/Intro';
 import Steps from './Steps/Steps';
 import Comment from './Comment/Comment';
+import { useParams } from 'react-router-dom';
+import API from '../../../api/API';
+import ENDPOINT from '../../../api/ENDPOINT';
 
 const PostDetailPage = () => {
+  const params = useParams();
+
+  const [postInfo, setPostInfo] = useState('');
+  console.log(postInfo);
+
+  // 레시피 상세 조회
+  const getPostInfo = useCallback(() => {
+    API(`${ENDPOINT.POSTS}/${params.postid}`, 'GET')
+      .then((res) => {
+        console.log(res);
+        setPostInfo(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [params]);
+
+  useEffect(() => {
+    getPostInfo();
+  }, [getPostInfo]);
+
   return (
     <>
-      <MainHeadingLayout MainheadingName='레시피 상세 페이지' />
-      <Intro />
-      <Steps />
-      <Comment />
+      <MainHeadingLayout mainheadingName='레시피 상세 페이지' />
+      <Intro postInfo={postInfo} />
+      <Steps postInfo={postInfo} />
+      <Comment postInfo={postInfo} getPostInfo={getPostInfo} />
     </>
   );
 };
