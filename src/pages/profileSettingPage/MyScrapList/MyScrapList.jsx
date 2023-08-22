cee58d27a5372fb2d18f720898d1af130893d999
euiho kim <ehkim@elicer.com>
2023-08-22 18:15:50 +0900

    feat: 내 스크랩 기능 추가

--- src/pages/profileSettingPage/MyScrapList/MyScrapList.jsx
+++ src/pages/profileSettingPage/MyScrapList/MyScrapList.jsx
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Post from '../../../components/common/post/Post';
 import { styled } from 'styled-components';
+import API from '../../../api/API';
+import ENDPOINT from './../../../api/ENDPOINT';
+import MainHeadingLayout from '../../../components/common/layout/MainHeadingLayout/MainHeadingLayout';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../../components/common/Button/Button';
+import { mediaMaxWidth } from '../../../styles/GlobalStyle';
+
+const categories = ['프로필 수정', '내 스크랩'];
 
 const MyScrapList = () => {
+  const [postInfo, setPostInfo] = useState([]);
+  // console.log(postInfo);
+
+  const navigate = useNavigate();
+
+  const [selectedMenu, setSelectedMenu] = useState('내 스크랩');
+
+  // 클릭 시, 해당 페이지 이동
+  const onClickMovePageHandler = (category) => {
+    if (category === '프로필 수정') {
+      navigate('/profile/edit');
+    } else if (category === '내 스크랩') {
+      navigate('/profile/myscrap');
+    }
+  };
+
+  const getUserInfo = useCallback(() => {
+    API(`${ENDPOINT.GET_USER_SCRAPS_INFO}`, 'GET')
+      .then((res) => {
+        console.log(res);
+        setPostInfo(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    getUserInfo();
+  }, [getUserInfo]);
+
   return (
     <>
+      <MainHeadingLayout mainheadingName='내 프로필 관리' />
       <Container>
-        <Post accountname='test1' />
-        <Post accountname='test2' />
-        <Post accountname='test3' />
+        <Nav>
+          <Ul>
+            {categories.map((category) => (
+              <Li
+                key={category}
+                $selectedMenu={selectedMenu === category}
+                onClick={() => {
+                  setSelectedMenu(category);
+                  onClickMovePageHandler(category);
+                }}
+              >
+                <Button type='category' active={selectedMenu === category}>
+                  {category}
+                </Button>
+              </Li>
+            ))}
+          </Ul>
+        </Nav>
       </Container>
+      <PostContainer>
+        {postInfo ? (
+          postInfo.map((post) => <Post key={post._id} postInfo={post} />)
+        ) : (
+          <p>아직 스크랩한 게시물이 없습니다.</p>
+        )}
+      </PostContainer>
     </>
   );
 };
@@ -17,8 +77,30 @@ const MyScrapList = () => {
 export default MyScrapList;
 
 const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  border-radius: 1rem;
+`;
+
+const Ul = styled.nav`
+  display: flex;
+  gap: 2rem;
+`;
+
+const Li = styled.li``;
+
+const PostContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 2rem;
   margin-top: 2rem;
+
+  @media (max-width: ${mediaMaxWidth}) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
--- src/pages/profileSettingPage/ProfileSettingPage.jsx
+++ src/pages/profileSettingPage/ProfileSettingPage.jsx
@@ -1,42 +1,60 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { styled } from 'styled-components';
 import MainHeadingLayout from './../../components/common/layout/MainHeadingLayout/MainHeadingLayout';
 import Button from '../../components/common/Button/Button';
 import ProfileEdit from './ProfileEdit/ProfileEdit';
-import MyScrapList from './MyScrapList/MyScrapList';
+import { useNavigate } from 'react-router-dom';
 
 const categories = ['프로필 수정', '내 스크랩'];
 
 const ProfileSettingPage = () => {
+  const navigate = useNavigate();
+
   const [selectedMenu, setSelectedMenu] = useState('프로필 수정');
+
+  // 클릭 시, 해당 페이지 이동
+  const onClickMovePageHandler = (category) => {
+    if (category === '프로필 수정') {
+      navigate('/profile/edit');
+    } else if (category === '내 스크랩') {
+      navigate('/profile/myscrap');
+    }
+  };
 
   return (
     <>
-      <MainHeadingLayout MainheadingName='내 프로필 관리' />
-      <Nav>
-        <Ul>
-          {categories.map((category) => (
-            <Li
-              key={category}
-              $selectedMenu={selectedMenu === category}
-              onClick={() => {
-                setSelectedMenu(category);
-              }}
-            >
-              <Button type='category' active={selectedMenu === category}>
-                {category}
-              </Button>
-            </Li>
-          ))}
-        </Ul>
-      </Nav>
-      {selectedMenu === '프로필 수정' && <ProfileEdit />}
-      {selectedMenu === '내 스크랩' && <MyScrapList />}
+      <MainHeadingLayout mainheadingName='내 프로필 관리' />
+      <Container>
+        <Nav>
+          <Ul>
+            {categories.map((category) => (
+              <Li
+                key={category}
+                $selectedMenu={selectedMenu === category}
+                onClick={() => {
+                  setSelectedMenu(category);
+                  onClickMovePageHandler(category);
+                }}
+              >
+                <Button type='category' active={selectedMenu === category}>
+                  {category}
+                </Button>
+              </Li>
+            ))}
+          </Ul>
+        </Nav>
+      </Container>
+      <ProfileEdit />
     </>
   );
 };
 
 export default ProfileSettingPage;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Nav = styled.nav`
   display: flex;
@@ -49,13 +67,4 @@ const Ul = styled.nav`
   gap: 2rem;
 `;
 
-const Li = styled.li`
-  /* font-size: var(--fs-md);
-  font-weight: 700;
-  padding: 1rem;
-  background: ${(props) => (props.$selectedMenu ? 'var(--main-color)' : 'var(--text-color)')};
-  color: ${(props) => (props.$selectedMenu ? 'var(--text-white-color)' : 'var(--text-color)')};
-  border: 1px solid ${(props) => (props.$selectedMenu ? 'var(--main-color)' : 'var(--border-color)')};
-  border-radius: 1rem;
-  cursor: pointer; */
-`;
+const Li = styled.li``;
