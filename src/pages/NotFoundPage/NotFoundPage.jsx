bb05438a4b4dbef9d3da46511aecc28438134e13
euiho kim <ehkim@elicer.com>
2023-09-05 16:47:34 +0900

    design: NotFoundPage 컴포넌트 레이아웃 수정

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
 const NotFoundpage = () => {
-  return <div>NotFoundpage</div>;
+  return (
+    <>
+      <Container>Page not found</Container>
+    </>
+  );
 };
 
 export default NotFoundpage;
+
+const Container = styled.div`
+  font-size: var(--fs-xl);
+  text-align: center;
+  margin: auto;
+`;
